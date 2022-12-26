827a083435173dbaf17b3fa390851efb48e9ddbd
Siddharth9080 <antima22@gmail.com>
2022-12-26 18:54:55 +0530

    RX

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Section from './components/Section';
 import ComingSoon from './components/ComingSoon';
 import Navbar from './components/navbar';
 import ES300h from "./Pictures/ES300h.jpg"
+import RX450h from "./Pictures/RX450h.jpg";
 import LC500h from "./Pictures/LC500h.jpg"
 import LS500h from "./Pictures/LS500h.jpg"
 import LX600 from "./Pictures/LX600.jpg"
@@ -16,7 +17,8 @@ function App() {
       <Section name="ES300h" image={ES300h} color="#000" description="Self Charging Hybrid Sedan"/>
       <Section name="LS500h" image={LS500h} color="#fff" description="Self Charging Hybrid Luxury Sedan"/>
       <Section name="LC500h" image={LC500h} color="#fff" description="Self Charging Hybrid Luxury Coupé"/>
-      <ComingSoon name="LX600" image={LX600} color="#fff" description="Luxury Full-Size SUV"/>
+      <Section name="RX500h" image={RX450h} color="#fff" description="Mid-Size Luxury Crossover"/>
+      <ComingSoon name="LX600" image={LX600} color="#fff" description="Full-Size Luxury SUV"/>
     </div>
   );
 }
